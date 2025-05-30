359d2e8a3e81f9632064edf890936875fbb0fc9b
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-05-31 01:15:37 +0600

    added DTO for create and update storeCategory

--- src/store-category/dto/create-store-category.dto.ts
+++ src/store-category/dto/create-store-category.dto.ts
@@ -1,1 +1,11 @@
-export class CreateStoreCategoryDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { StoreType } from '@prisma/client';
+import { IsEnum, IsString } from 'class-validator';
+
+export class CreateStoreCategoryDto {
+  @IsString()
+  categoryName: string;
+
+  @IsEnum(StoreType)
+  storeType: StoreType;
+}
--- src/store-category/dto/update-store-category.dto.ts
+++ src/store-category/dto/update-store-category.dto.ts
@@ -1,6 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateStoreCategoryDto } from './create-store-category.dto';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { StoreType } from '@prisma/client';
 
 export class UpdateStoreCategoryDto extends PartialType(
   CreateStoreCategoryDto,
-) {}
+) {
+  @IsOptional()
+  @IsString()
+  categoryName: string;
+
+  @IsOptional()
+  @IsEnum(StoreType)
+  storeType: StoreType;
+}
