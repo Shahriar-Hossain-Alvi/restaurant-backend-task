5e0a280b96cb70fa5c5b257e74d9a0be4a7d0d1c
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-05-31 02:32:31 +0600

    created api to create storeCategory

--- src/store-category/store-category.controller.ts
+++ src/store-category/store-category.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { StoreCategoryService } from './store-category.service';
 import { CreateStoreCategoryDto } from './dto/create-store-category.dto';
-import { UpdateStoreCategoryDto } from './dto/update-store-category.dto';
 
 @Controller('store-category')
 export class StoreCategoryController {
@@ -25,21 +16,21 @@ export class StoreCategoryController {
     return this.storeCategoryService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storeCategoryService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.storeCategoryService.findOne(id);
+  // }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateStoreCategoryDto: UpdateStoreCategoryDto,
-  ) {
-    return this.storeCategoryService.update(+id, updateStoreCategoryDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateStoreCategoryDto: UpdateStoreCategoryDto,
+  // ) {
+  //   return this.storeCategoryService.update(id, updateStoreCategoryDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storeCategoryService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.storeCategoryService.remove(id);
+  // }
 }
--- src/store-category/store-category.service.ts
+++ src/store-category/store-category.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateStoreCategoryDto } from './dto/create-store-category.dto';
-import { UpdateStoreCategoryDto } from './dto/update-store-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class StoreCategoryService {
-  create(createStoreCategoryDto: CreateStoreCategoryDto) {
-    return 'This action adds a new storeCategory';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createStoreCategoryDto: CreateStoreCategoryDto) {
+    const existingStoreCategory = await this.prisma.storeCategory.findUnique({
+      where: {
+        categoryName: createStoreCategoryDto.categoryName,
+      },
+    });
+
+    if (existingStoreCategory) {
+      throw new BadRequestException(
+        `A ${createStoreCategoryDto.categoryName} category already exists`,
+      );
+    }
+
+    const newStoreCategory = await this.prisma.storeCategory.create({
+      data: { ...createStoreCategoryDto },
+    });
+
+    return newStoreCategory;
   }
 
-  findAll() {
-    return `This action returns all storeCategory`;
+  async findAll() {
+    const allStoreCategory = await this.prisma.storeCategory.findMany({
+      orderBy: { categoryName: 'asc' },
+    });
+
+    return allStoreCategory;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} storeCategory`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} storeCategory`;
+  // }
 
-  update(id: number, updateStoreCategoryDto: UpdateStoreCategoryDto) {
-    return `This action updates a #${id} storeCategory`;
-  }
+  // update(id: number, updateStoreCategoryDto: UpdateStoreCategoryDto) {
+  //   return `This action updates a #${id} storeCategory`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} storeCategory`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} storeCategory`;
+  // }
 }
